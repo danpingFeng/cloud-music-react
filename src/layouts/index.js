49d9c994738050a79902639bdbfb0f5d47923268
danpingFeng <785147539@qq.com>
2019-12-21 23:53:29 +0800

    头部ready

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import styles from './index.css';
+import styles from './index.less';
+
+import Top from '../components/Top';
+import Tab from '../components/Tab';
 
 // 全局路由
 function BasicLayout(props) {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Header</h1>
-      {props.children}
-    </div>
-  );
+    return (
+        <div className={styles.normal}>
+            <Top></Top>
+            <Tab></Tab>
+            {props.children}
+        </div>
+    );
 }
 
 export default React.memo(BasicLayout);
