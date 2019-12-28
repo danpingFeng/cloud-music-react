1b58d6b88b1e192dec15a298875ffc13f950799e
danpingFeng <785147539@qq.com>
2019-12-28 23:23:54 +0800

    add 歌手页面 顶部横线列表

--- src/pages/Singers/index.js
+++ src/pages/Singers/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Horizen from '@/components/horizenItem';
+import {NavContainer} from './style';
+import {categoryTypes, alphaTypes} from '@/config/index';
+
 function Singers() {
+    let [category, setCategory] = useState('');
+    let [alpha, setAlpha] = useState('');
+
+    let handleUpdateAlpha = val => {
+        setAlpha(val);
+    }
+
+    let handleUpdateCategory = val => {
+        setCategory(val);
+    }
+
+
     return (
-        <div>
-            Singers
-        </div>
+        <NavContainer>
+            <Horizen list={categoryTypes} title={"分类(默认热门):"} handleClick={val => handleUpdateCategory(val)} oldVal={category} ></Horizen>
+            <Horizen list={alphaTypes} title={"首字母:"} handleClick={val => handleUpdateAlpha(val)} oldVal={alpha} ></Horizen>
+        </NavContainer >
     );
 }
 
